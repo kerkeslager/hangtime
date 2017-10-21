dc349fd7c47e867a03d97ff84e50e22f6ef98d63
David Kerkeslager <kerkeslager@gmail.com>
2017-10-21 00:39:05 -0400

    Better workout selection

--- static_source/js/workout-selector.js
+++ static_source/js/workout-selector.js
@@ -8,26 +8,36 @@ export default class WorkoutSelector extends React.Component {
     super(props);
 
     this.state = {
+      error: null,
       selectedWorkout: null
     };
   }
 
   onSelectChanged(e) {
     this.setState({
+      error: null,
       selectedWorkout: WORKOUTS[e.target.value]
     });
   }
 
   onStartClicked() {
-    this.props.onSelected(this.state.selectedWorkout);
+    if(this.state.selectedWorkout) {
+      this.props.onSelected(this.state.selectedWorkout);
+    } else {
+      this.setState({
+        error: 'You must select a workout to continue'
+      });
+    }
   }
 
   renderOptions() {
-    return WORKOUTS.map((workout, workoutIndex) => {
+    return [
+      <option key={ 'workout-default' }></option>
+    ].concat(WORKOUTS.map((workout, workoutIndex) => {
       return <option key={ 'workout-' + workoutIndex } value={ workoutIndex }>
         { workout.name }
       </option>;
-    });
+    }));
   }
 
   render() {
@@ -35,6 +45,7 @@ export default class WorkoutSelector extends React.Component {
       <select onChange={ this.onSelectChanged.bind(this) }>
         { this.renderOptions() }
       </select>
+      <p>{ this.state.error }</p>
       <button onClick={ this.onStartClicked.bind(this) }>{ "Start" }</button>
     </div>;
   }
